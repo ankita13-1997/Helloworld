369250c033fd5d5d1f4604bf91eb428fbcc02686
ankita13-1997 <parhiankita@gmail.com>
2021-02-01 11:13:56 +0530

    display an msg

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,13 @@ System.out.println("hello u branch!!");
 
 System.out.println("push");
 
-System.out.println("hello u r not branch!!, changed the program in remote"); 
+System.out.println("hello u r not branch!!, changed the program in remote");
+HelloWorld m= new HelloWorld();
+m.t(); 
 }
+  public void t()
+  {
+    System.out.println("hello done of it");
 
+  }            
 }
